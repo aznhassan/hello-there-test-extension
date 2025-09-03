846d5dbf21f04ae9c2a8191ecebe9a99d5624444
Hassan Sufi <hasufi@microsoft.com>
2025-09-03 14:36:03 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+//% color=#0000FF 
+//% icon="\uf074"
+//% block="Custom"
+//% group="micro:bit (V2)"
+namespace custom {
+    //% blockId="bar" block="bar %a %s|"
+    //% shim=custom::bar
+    //% n=number s=string
+    //% group="micro:bit (V2)"
+    export function bar(n: number, s: string): void {
+        // Per https://github.com/microsoft/pxt-microbit/issues/4292
+        0;
+    }
+}
