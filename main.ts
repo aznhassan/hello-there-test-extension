f25225eab7b4209127441906cb44027f66167a8c
Hassan Sufi <hasufi@microsoft.com>
2025-09-03 16:10:13 -0700

    Remove group, update category name

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
 //% color=#0000FF 
 //% icon="\uf074"
-//% block="Custom"
-//% group="micro:bit (V2)"
+//% block="My Custom Blocks"
 namespace custom {
     //% blockId="myFunction" block="myFunction %a %s|"
     //% shim=custom::myFunction
     //% a=number b=number
-    //% group="micro:bit (V2)"
     export function myFunction(a: number, b: number): number {
         // Per https://github.com/microsoft/pxt-microbit/issues/4292
         return a + b;
